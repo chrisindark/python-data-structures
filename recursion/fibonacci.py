eaf5a86e06dd2e6d6184419e221fd620392cc3d3
Christopher Paul <christopherp@mindfiresolutions.com>
2018-08-16 12:47:04 +0530

    commited code for recursion programs

---
+++ recursion/fibonacci.py
@@ -0,0 +1,17 @@
+# 0 1 1 2 3 5 8 13 21 ....
+
+numbers = []
+
+def fibonacci(a, b, n):
+    if numbers == []:
+        numbers.append(0)
+
+    if n == 0:
+        numbers.append(b)
+        return numbers
+    else:
+        numbers.append(b)
+        return fibonacci(b, a+b, n - 1)
+
+print(fibonacci(0, 1, 10))
+print(fibonacci(0, 1, 10))
